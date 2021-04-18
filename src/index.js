479e3049bd71af3750327e8e87b20d803fb6f0d9
Ravi <dev.raviraghav93@gmail.com>
2021-04-18 22:16:29 +0530

    Project Initialize commit
    Project Initialize commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Controller from './screens/Controller';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Controller />
   </React.StrictMode>,
   document.getElementById('root')
 );
